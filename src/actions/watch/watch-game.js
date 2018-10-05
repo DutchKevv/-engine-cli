3ec8c99223940a229838ac64943ec58cfccee5af
unknown <kewin@frontend-freelance.com>
2018-10-05 04:25:34 +0200

    w

--- src/actions/watch/watch-game.js
+++ src/actions/watch/watch-game.js
@@ -1,3 +1,4 @@
+require("colors");
 const fs = require("fs");
 const path = require("path");
 const webpack = require("webpack");
@@ -7,10 +8,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const oWebpackConfig = require('../../webpack/webpack.dev');
 const npmLink = require('../npm-link/npm-link');
 
-const PATH_ENGINE_CORE = path.resolve(__dirname, '../../../../engine-core');
-const PATH_ENGINE_CLI = path.resolve(__dirname, '../../../../engine-cli');
-const PATH_PROJECTS_SRC = path.resolve(__dirname, '../../../../data/static/projects/');
-const PATH_PROJECTS_DIST = path.resolve(__dirname, '../../../../data/public/projects/');
+const PATH_ROOT = path.resolve(__dirname, '../../../../');
+const PATH_ENGINE_CORE = path.resolve(PATH_ROOT, 'client-core');
+const PATH_ENGINE_EDITOR = path.resolve(PATH_ROOT, 'client-editor');
+const PATH_ENGINE_SHARED = path.resolve(PATH_ROOT, 'shared');
+const PATH_PROJECT_DATA = path.resolve(process.cwd(), '../../../'); // TODO - config custom data path
+const PATH_PROJECTS_SRC = path.resolve(PATH_PROJECT_DATA, 'static/projects');
+const PATH_PROJECTS_DIST = path.resolve(PATH_PROJECT_DATA, 'public/projects');
+
+console.log('pwd', PATH_ROOT);
 
 module.exports = function (gameId, clean = true) {
 
@@ -20,18 +26,22 @@ module.exports = function (gameId, clean = true) {
     const configPath = path.join(srcPath, 'config.json');
     const config = JSON.parse(fs.readFileSync(configPath));
 
+    process.chdir(srcPath);
+
     // link core and cli to active project
     // TODO - whats the ultimate way to do this? This feels 'spooky'
     await Promise.all([
-      npmLink('engine-core', PATH_ENGINE_CORE, srcPath),
-      npmLink('engine-cli', PATH_ENGINE_CLI, srcPath)
+      npmLink('@foreplay/client-core', PATH_ENGINE_CORE, srcPath),
+      npmLink('@foreplay/client-editor', PATH_ENGINE_EDITOR, srcPath),
+      npmLink('@foreplay/shared', PATH_ENGINE_SHARED, srcPath),
     ]);
-
+    
     // merge webpackConfig with custom options
     const webpackConfig = Object.assign({}, oWebpackConfig);
 
     // set context to engine-cli
     webpackConfig.context = path.resolve(__dirname, '../../../');
+    // webpackConfig.context = path.resolve(__dirname, '../../../');
 
     setWebpackEntries(webpackConfig, config.compile, srcPath);
     addEditor(webpackConfig, config.compile, srcPath)
@@ -58,10 +68,12 @@ module.exports = function (gameId, clean = true) {
         // Handle errors here
         const statsJson = stats.toJson();
 
-        statsJson.errors.forEach(error => console.log('\n' + '\x1b[31m', error.toString() + '\n'));
+        statsJson.errors.forEach(error => console.log('\n' + error.toString().red + '\n'));
+
+        return;
       }
 
-      console.info('\n compiling done, no errors');
+      console.info('\n compiling done. 0 errors');
 
       resolve();
     });
@@ -69,7 +81,7 @@ module.exports = function (gameId, clean = true) {
 };
 
 function addEditor(webpackConfig, compileOptions, projectPath) {
-  webpackConfig.entry['editor'] = path.resolve(projectPath, 'node_modules/engine-core/src/editor/editor.ts');
+  webpackConfig.entry['editor'] = path.resolve(projectPath, 'node_modules/@foreplay/client-editor/src/editor.ts');
 }
 
 function setCopyFilesPlugin(webpackConfig, compileOptions, projectPath, destinationPath) {
